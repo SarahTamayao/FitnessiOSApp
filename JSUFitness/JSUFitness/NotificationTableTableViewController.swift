695c3a8663cfec0ced9b7a94d010b7d8b168d6a5
Chao <nickjc870111@gmail.com>
2022-03-22 22:43:09 -0500

    initialize the project

---
+++ JSUFitness/JSUFitness/NotificationTableTableViewController.swift
@@ -0,0 +1,40 @@
+//
+//  NotificationTableTableViewController.swift
+//  JSUFitness
+//
+//  Created by Chao Jiang on 3/22/22.
+//
+
+import UIKit
+
+class NotificationTableTableViewController: UITableViewController {
+    
+    var notificationList = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+}
