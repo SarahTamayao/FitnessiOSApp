3660ad1e02eea2d761dadf326585e9a6eeecb6be
Chao <nickjc870111@gmail.com>
2022-03-23 10:37:52 -0500

    added profile viewcontroller into the project

---
+++ JSUFitness/JSUFitness/ProfileViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileViewController.swift
+//  JSUFitness
+//
+//  Created by Chao Jiang on 3/23/22.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
