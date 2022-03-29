96bd122b40227a031d4b6f21fced834c16594c4b
Chao <nickjc870111@gmail.com>
2022-03-29 11:56:15 -0500

    Feature issue Solved  1. created tableViewCell for scheduleTableView  2. set up the cell layout  3. finished the feature that user can tap a cell to go to new ScheduleDetailViewController

---
+++ JSUFitness/JSUFitness/Schedule/ScheduleDetailViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ScheduleDetailViewController.swift
+//  JSUFitness
+//
+//  Created by Chao Jiang on 3/29/22.
+//
+
+import UIKit
+
+class ScheduleDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        self.title = "Detail"
+    }
+    
+
+}
