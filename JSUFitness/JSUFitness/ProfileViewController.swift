88c73216a6faccf7e4f95636750a4b498e57405d
Binyam Assefa <binia6181@gmail.com>
2022-03-31 18:30:08 -0500

    added the login and edit functionality

--- JSUFitness/JSUFitness/ProfileViewController.swift
+++ JSUFitness/JSUFitness/ProfileViewController.swift
@@ -9,10 +9,21 @@ import UIKit
 
 class ProfileViewController: UIViewController {
 
+    @IBOutlet weak var FirstnameLabel: UILabel!
+    @IBOutlet weak var LastnameLabel: UILabel!
+    @IBOutlet weak var roleLabel: UILabel!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var emailLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func LogoutButton(_ sender: Any) {
+    }
+    
+    @IBAction func EditButton(_ sender: Any) {
+    }
 }
