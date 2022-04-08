85dd52b23439ade6d22830d20640b1ddb181a0d8
Binyam Assefa <binia6181@gmail.com>
2022-04-07 19:01:17 -0500

    Added profile and edit view controllers

---
+++ JSUFitness/JSUFitness/EditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  EditViewController.swift
+//  JSUFitness
+//
+//  Created by 1234 on 4/7/22.
+//
+
+import UIKit
+
+class EditViewController: UIViewController {
+
+    @IBOutlet weak var firstNameTF: UITextField!
+    @IBOutlet weak var lastNameTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    @IBOutlet weak var phoneTF: UITextField!
+    @IBOutlet weak var roleChangeTF: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func doneEditingPressed(_ sender: UIButton) {
+        
+        
+    }
+    
+}
